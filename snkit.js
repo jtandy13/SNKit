355cb28d9d440587732d64742a5549f016a83c32
james.tandy <james.tandy@servicenow.com>
2017-11-16 22:39:55 +1100

    performance and components panels added

--- snkit.js
+++ snkit.js
@@ -1,6 +1,6 @@
 var SNKit = (() => {
   return {
-    version: "1.0.0",
+    version: "2.0.0",
     getWidgetDetails: (callback) => {
       var details = [];
       var spWidgets = document.querySelectorAll("[widget='widget']");
